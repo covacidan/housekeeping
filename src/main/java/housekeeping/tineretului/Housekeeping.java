7e72d50531a2d119f72b5dae180cd73d08f221e5
Covaci Dan <covacidan@gmail.com>
2022-08-24 21:04:01 +0300

    remote tomcat config

--- src/main/java/housekeeping/tineretului/Housekeeping.java
+++ src/main/java/housekeeping/tineretului/Housekeeping.java
@@ -3,10 +3,17 @@ package housekeeping.tineretului;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class Housekeeping {
+public class Housekeeping extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Housekeeping.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Housekeeping.class, args);
