d00177b3e4d93e5cf28ceeba61db3f87c0375dad
Covaci Dan <covacidan@gmail.com>
2022-09-06 15:37:43 +0300

    dockerfile modified

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM tomcat:latest
+FROM openjdk:8-jdk-alpine
 
-ADD target/housekeeping.war /usr/local/tomcat/webapps/
-EXPOSE 8080
-CMD ["catalina.sh", "run"]
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/housekeeping/tineretului/Housekeeping.java
+++ src/main/java/housekeeping/tineretului/Housekeeping.java
@@ -3,17 +3,10 @@ package housekeeping.tineretului;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class Housekeeping extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Housekeeping.class);
-	}
+public class Housekeeping /*extends SpringBootServletInitializer*/ {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Housekeeping.class, args);
